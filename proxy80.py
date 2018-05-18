4e5de26bc5149b459e5eab5dbbe37e9da023d8aa
visonsoft13 <38098742+visonsoft13@users.noreply.github.com>
2018-05-18 07:24:22 -0300

    Update proxy80.py

--- proxy80.py
+++ proxy80.py
@@ -12,7 +12,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:144'
-RESPONSE = 'HTTP/1.1 200 TwoSSH\r\n\r\n'
+RESPONSE = 'HTTP/1.1 200 VisonSoft\r\n\r\n'
 #RESPONSE = 'HTTP/1.1 200 Hello_World!\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'  # lint:ok
 
 
